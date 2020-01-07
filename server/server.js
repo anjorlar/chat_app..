01eedb1d40f5ca239150a621fb4487a208c8e9ae
anjorlar <adeanjola@gmail.com>
2020-01-07 17:25:12 +0100

    commas

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ io.on('connection', function (socket) {
 
     socket.on('disconnect', function () {
         console.log('User was disconnected')
-    })
+    });
 });
 
 server.listen(port, () => {
