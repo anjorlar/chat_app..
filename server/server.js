338b36efce78cd01bfc764fde6d8a7d22b7b6ec6
anjorlar <adeanjola@gmail.com>
2020-01-12 11:05:47 +0100

    file update

--- server/server.js
+++ server/server.js
@@ -5,9 +5,9 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 let app = express();
-let server = http.createServer(app)
+let server = http.createServer(app);
 let io = socketIO(server);
 
 app.use(express.static(publicPath));
